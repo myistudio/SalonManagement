7881569d1f2fff71088959a1446c5689036e4be6
myistudio <151857643+myistudio@users.noreply.github.com>
2025-08-02 03:03:58 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ set -e
 # Configuration
 APP_NAME="salonpro"
 APP_DIR="/var/www/salonpro"
-REPO_URL="https://github.com/myistudio/SalonManagement.git"
+REPO_URL="https://github.com/myistudio/SalonManagement"
 DB_NAME="salonpro"
 DB_USER="salonpro_user"
 DB_PASSWORD="Veenails@2!"
